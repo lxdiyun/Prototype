4bc6010e4d91e11c3a73b4c021bbd31f42b483d1
Adrain Lee <lxdiyun@gmail.com>
2012-01-22 23:50:07 +0800

    Remove core data to fix crash on reopen issue(ipod touch)

--- Prototype/Prototype/AppDelegate.m
+++ Prototype/Prototype/AppDelegate.m
@@ -43,9 +43,6 @@ static UIViewController *MSWJ_PAGE_INSTANCE[MSWJ_PAGE_QUANTITY] = {nil};
 @implementation AppDelegate
 
 @synthesize window = _window;
-@synthesize managedObjectContext = __managedObjectContext;
-@synthesize managedObjectModel = __managedObjectModel;
-@synthesize persistentStoreCoordinator = __persistentStoreCoordinator;
 @synthesize tabco = _tabco;
 
 #pragma mark - life circle
@@ -86,9 +83,6 @@ static UIViewController *MSWJ_PAGE_INSTANCE[MSWJ_PAGE_QUANTITY] = {nil};
 	[self releasePageInstance];
 	
 	[_window release];
-	[__managedObjectContext release];
-	[__managedObjectModel release];
-	[__persistentStoreCoordinator release];
 	[super dealloc];
 }
 
@@ -141,6 +135,7 @@ static UIViewController *MSWJ_PAGE_INSTANCE[MSWJ_PAGE_QUANTITY] = {nil};
 - (void)applicationWillResignActive:(UIApplication *)application
 {
 	/*
+	 TODO
 	 Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 	 Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 	 */
@@ -166,110 +161,6 @@ static UIViewController *MSWJ_PAGE_INSTANCE[MSWJ_PAGE_QUANTITY] = {nil};
 - (void)applicationWillTerminate:(UIApplication *)application
 {
 	[ObjectSaver saveAll];
-	[self saveContext];
-}
-
-- (void)saveContext
-{
-	NSError *error = nil;
-	NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
-	if (managedObjectContext != nil)
-	{
-		if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error])
-		{
-			/*
-			 Replace this implementation with code to handle the error appropriately.
-			 
-			 abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-			 */
-			LOG(@"Unresolved error %@, %@", error, [error userInfo]);
-			abort();
-		} 
-	}
-}
-
-#pragma mark - Core Data stack
-
-/**
- Returns the managed object context for the application.
- If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
- */
-- (NSManagedObjectContext *)managedObjectContext
-{
-	if (__managedObjectContext != nil)
-	{
-		return __managedObjectContext;
-	}
-	
-	NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-	if (coordinator != nil)
-	{
-		__managedObjectContext = [[NSManagedObjectContext alloc] init];
-		[__managedObjectContext setPersistentStoreCoordinator:coordinator];
-	}
-	return __managedObjectContext;
-}
-
-/**
- Returns the managed object model for the application.
- If the model doesn't already exist, it is created from the application's model.
- */
-- (NSManagedObjectModel *)managedObjectModel
-{
-	if (__managedObjectModel != nil)
-	{
-		return __managedObjectModel;
-	}
-	NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Prototype" withExtension:@"momd"];
-	__managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-	return __managedObjectModel;
-}
-
-/**
- Returns the persistent store coordinator for the application.
- If the coordinator doesn't already exist, it is created and the application's store added to it.
- */
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-{
-	if (__persistentStoreCoordinator != nil)
-	{
-		return __persistentStoreCoordinator;
-	}
-	
-	NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Prototype.sqlite"];
-	
-	NSError *error = nil;
-	__persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-	if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error])
-	{
-		/*
-		 Replace this implementation with code to handle the error appropriately.
-		 
-		 abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-		 
-		 Typical reasons for an error here include:
-		 * The persistent store is not accessible;
-		 * The schema for the persistent store is incompatible with current managed object model.
-		 Check the error message to determine what the actual problem was.
-		 
-		 
-		 If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
-		 
-		 If you encounter schema incompatibility errors during development, you can reduce their frequency by:
-		 * Simply deleting the existing store:
-		 [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
-		 
-		 * Performing automatic lightweight migration by passing the following dictionary as the options parameter: 
-		 [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption, [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
-		 
-		 Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
-		 
-		 */
-		LOG(@"Unresolved error %@, %@", error, [error userInfo]);
-		abort();
-	}    
-	
-	return __persistentStoreCoordinator;
 }
 
 #pragma mark - Application's Documents directory
